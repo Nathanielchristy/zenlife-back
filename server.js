b62a2a0b07f0ea430c9ef2865428f0614681fe8f
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2024-06-10 18:21:09 +0530

    cors

--- server.js
+++ server.js
@@ -19,40 +19,6 @@ const app = express();
 const server = http.createServer(app);
 
 // Allow requests from a specific origin
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-const socketIO = new Server(server, {
-  cors: {
-    origin: "http://3.29.133.152:4173",
-    credentials: true,
-    allowedHeaders: "Access-Control-Allow-Credentials",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
-socketIO.use(async (socket, next) => {
-  if (config.allowedOrigins.includes(socket.request.headers.origin)) {
-    return next();
-  }
-  return next(new Error("Not allowed by CORS"));
-});
-
-socketIO.on("connection", (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on("disconnect", () => {
-    console.log("🔥: A user disconnected");
-  });
-
-  socket.on("event", (event) => {
-    console.log("received event", event);
-    io.emit("event", event); // Broadcast the event to all clients
-  });
-});
-
-// CORS configuration
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -68,13 +34,40 @@ app.use(
   })
 );
 
-//Middlewares
+// Socket.IO configuration
+const socketIO = new Server(server, {
+  cors: {
+    origin: config.allowedOrigins, // Array of allowed origins
+    credentials: true,
+  },
+});
+
+socketIO.use(async (socket, next) => {
+  if (config.allowedOrigins.includes(socket.request.headers.origin)) {
+    return next();
+  }
+  return next(new Error("Not allowed by CORS"));
+});
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+
+  socket.on("event", (event) => {
+    console.log("received event", event);
+    socketIO.emit("event", event); // Broadcast the event to all clients
+  });
+});
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Route middleware
+// Route middleware
 app.use("/api/authUser", userAuthRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/users", userDataRoutes);
@@ -82,18 +75,13 @@ app.use("/api/roles", roleRoutes);
 app.use("/api/dashboard", jobRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/jobstatus", jobStatusRoutes);
-// //Routes
-// app.get("/", (req, res) => {
-//   res.send("Home Page");
-// });
 
-//Error Middle ware
+// Error Middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to Db and start server
-
+// Connect to DB and start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
