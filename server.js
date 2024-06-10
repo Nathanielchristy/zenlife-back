21795e03f9b08d2672e0ff9597a9452394a8dee3
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2024-06-10 18:12:03 +0530

    update ip

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 // );
 const socketIO = new Server(server, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "http://3.29.133.152:4173",
     credentials: true,
     allowedHeaders: "Access-Control-Allow-Credentials",
     methods: ["GET", "POST", "PUT", "DELETE"],
