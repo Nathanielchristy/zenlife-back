fe435534155974275c8681e7c1c9f806ff69eb19
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2026-03-06 01:29:07 +0530

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -9,10 +9,11 @@ module.exports = {
     "http://localhost:4173",
     "http://192.168.0.113:5174",
     "http://172.26.16.1:5174",
-    "http://3.74.45.200:4173/",
+    "http://3.74.45.200:4173",
     "https://zenlife-front.vercel.app",
     "http://192.168.56.1:5174", // replace with your local IP address
   ],
   // Add any other configuration options here (e.g., secret keys)
 };
+
 
